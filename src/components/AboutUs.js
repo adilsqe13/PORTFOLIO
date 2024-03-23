92d7856fdb57ee3830695e926829370b0c7f17b4
adilsqe13 <adilsqe13@gmail.com>
2024-03-24 03:03:42 +0530

    UI updates

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -44,7 +44,7 @@ export default function AboutUs(){
 
             <div className="container md:mt-24 mt-16">
                 <div className="grid grid-cols-1 pb-8 text-center">
-                    <h3 className="mb-6 md:text-2xl text-xl md:leading-normal leading-normal font-semibold">Hobbies & Expertise</h3>
+                    <h3 className="mb-6 md:text-2xl text-xl md:leading-normal leading-normal font-semibold">Skills & Expertise</h3>
 
                     <p className="text-slate-400  mx-auto text-[15px]">{userData && userData.about.description}</p>
                 </div>
@@ -55,7 +55,7 @@ export default function AboutUs(){
                         return(
                             <div className="flex group shadow shadow-gray-200 dark:shadow-gray-800 dark:hover:shadow-gray-700 items-center p-3 rounded-lg bg-white dark:bg-slate-900" key={index}>
                             <div className="flex items-center justify-center h-[45px] min-w-[45px] -rotate-45 bg-amber-500/10 group-hover:bg-amber-500 text-amber-500 group-hover:text-white text-center rounded-xl me-5 transition-all duration-500">
-                                <img style={{backgroundColor: 'white'}} src={Icon} height={50} width={50} alt="icon" />
+                                <img className="bg-white dark:bg-slate-900"  src={Icon} height={50} width={50} alt="icon" />
                             </div>
                             <div className="flex-1">
                                 <h4 className="mb-0 text-[17px] font-medium">{item.name}</h4>
